35d4708c0ac3347a46ceb50aeffe2a679312e1fc
prokamlesh <98456619+prokamlesh@users.noreply.github.com>
2022-10-31 08:08:10 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -28,7 +28,8 @@ const Header = () => {
             </NavItem>
             <NavItem>
               <Link className="link"
-        to="/">
+        to="/task
+       ">
                 TASK
               </Link>
             </NavItem>
