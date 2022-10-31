6c90e474568e225bbd9920345f09eb3a125fbfdc
Kamlesh Mengu <guddykam9022@gmail.com>
2022-10-31 02:39:04 +0000

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,48 +1,39 @@
-import React from 'react';
+import React from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    NavbarText,
-  } from 'reactstrap';
-  import { Link ,useNavigate} from "react-router-dom";
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
-    
-    return(
-        <div>
-            
-            <h1>LOGO</h1>
-          
-            
-            <Navbar>
-        
-          
-            <NavItem>
-              <Link className="link"
-        to="/home">HOME</Link>
-            </NavItem>
-            <NavItem>
-              <Link className="link"
-        to="/">
-                TASK
-              </Link>
-            </NavItem>
-            <NavItem>
-              <Link className="link"
-        to="/user">USER</Link>
-            </NavItem>
-            
-          
-          
-        
+  return (
+    <div>
+      <h1>LOGO</h1>
+
+      <Navbar>
+        <NavItem>
+          <Link className="link" to="/home">
+            HOME
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link className="link" to="/task">
+            TASK
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link className="link" to="/user">
+            USER
+          </Link>
+        </NavItem>
       </Navbar>
-        </div>
-    );
-    
-}
+    </div>
+  );
+};
 export default Header;
